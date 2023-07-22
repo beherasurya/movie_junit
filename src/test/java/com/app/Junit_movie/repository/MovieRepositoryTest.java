fa7798bf560cdbbffd4ef37dc274f4cd9549c647
beherasurya <beherasuryakanta18@gmail.com>
2023-07-22 09:25:49 +0530

    getMovieById() test case is updated

--- src/test/java/com/app/Junit_movie/repository/MovieRepositoryTest.java
+++ src/test/java/com/app/Junit_movie/repository/MovieRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.app.Junit_movie.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,6 +21,7 @@ public class MovieRepositoryTest {
     MovieRepository movieRepository;
 
     @Test
+    @DisplayName("It should save the movie details in database")
     void save(){
 
         //Arrange
@@ -32,6 +36,55 @@ public class MovieRepositoryTest {
         assertNotNull(newMovie);
         
         assertNotNull(newMovie.getId());
+
+    }
+
+    @Test
+    @DisplayName("Movies list with size")
+    void getAllMovies(){
+
+         Movies avatarMovie= new Movies();
+        avatarMovie.setName("Avatar");
+        avatarMovie.setGenera("Action");
+        avatarMovie.setReleaseDate(LocalDate.of(2003, Month.JANUARY, 29));
+
+        //Act
+        movieRepository.save(avatarMovie);
+        //Assert
+
+         Movies titanicMovie= new Movies();
+        titanicMovie.setName("Titanic");
+        titanicMovie.setGenera("Romance");
+        titanicMovie.setReleaseDate(LocalDate.of(1999, Month.AUGUST, 31));
+
+        //Act
+        movieRepository.save(titanicMovie);
+        //Assert
+
+
+        List<Movies> movieList= movieRepository.findAll();
+        assertNotNull(movieList);
+        assertEquals(2, movieList.size());
+       
+    }
+
+    @Test
+    @DisplayName("Display Movie by ID")
+    void getMovieById(){
+
+        Movies titanicMovie= new Movies();
+        titanicMovie.setName("Titanic");
+
+        titanicMovie.setGenera("Romance");
+        titanicMovie.setReleaseDate(LocalDate.of(1999, Month.AUGUST, 31));
+
+        movieRepository.save(titanicMovie);
+        Movies existingMovie = movieRepository.findById(titanicMovie.getId()).get();
+
+        assertNotNull(existingMovie, "It should not be NULL");
+        assertEquals("Romance", existingMovie.getGenera(), "It will check Genera is same or not");
+
+
 
     }
 }
